ab4692d98d841d05af7744824eca4e40dc5e4496
newton-c <newton.christop@gmail.com>
2023-09-11 17:33:39 -0300

    exploratory visualizations

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -1,26 +1,31 @@
 library(tidyverse)
 library(readxl)
+library(ICplots)
 
-import_all_excels <- function(i) {
-  tmp_filepath <- paste0("data/", list.files("data/")[i])
-  temp_df <- read_xlsx(path = tmp_filepath, sheet = 2)
-}
+theme_set(theme_ic())
 
+provea <- read_xlsx("data/graphics 2 data.xlsx")[1:9] %>%
+  mutate(denuncias = as.numeric(`Denuncias/Discriminación según cuerpos de seguridad`))
 
-all_excels <- tibble()
+polisur <- provea %>%
+  filter(`Police Force` == "Municipal Police" &
+           (grepl("San Francisco", Municipality) == TRUE | Municipality == "PoliSur"))
 
-for (i in length(list.files("data/"))) {
-  tmp_df <- import_all_excels(i)
-  all_excels <- rbind(all_excels, tmp_df) %>%
-    filter(grepl("Fuente", Patrón) == FALSE)
-}
+polizulia <- provea %>%
+  filter(`Police Force` == "Regional Police" &
+         grepl("Zulia", State) == TRUE)
 
-all_excels_long <- pivot_longer(all_excels, cols = colnames(all_excels)[2:6],
-                                names_to = "Date")
-all_excels_long <- all_excels_long %>%
-  mutate(Date = as.numeric(sub("Ene.-Dic. ", "", all_excels_long$Date)))
+bar_chart <- ggplot() +
+  geom_col(data = polisur, mapping = aes(x = `Start Date`,
+                                         y = denuncias),
+           fill = "red", position = "jitter") +
+  geom_col(data = polizulia, mapping = aes(x = `Start Date`,
+                                           y = denuncias),
+           fill = "green", position = "jitter") +
+  hline
+bar_chart 
 
-
-ggplot(subset(all_excels_long, Patrón == "Torturas"), aes(x = Date, y = value)) +
-  geom_line(group = 1)
-         
+bar_chart <- ggplot() +
+  geom_col(data = polizulia, mapping = aes(x = `Start Date`,
+                                         y = denuncias))
+bar_chart
