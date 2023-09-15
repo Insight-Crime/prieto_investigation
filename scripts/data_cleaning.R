4d83e50f082ec2c438a67c74c99e0dc04dcce4b3
newton-c <newton.christop@gmail.com>
2023-09-15 15:22:04 -0300

    Finalizing English graphic

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -5,6 +5,7 @@ library(ggtext)
 
 theme_set(theme_ic())
 
+# Data munging ----------------------------------------------------------------
 provea <- read_xlsx("data/graphics 2 data.xlsx")[1:9] %>%
   mutate(denuncias = as.numeric(`Denuncias/Discriminación según cuerpos de seguridad`))
 
@@ -19,50 +20,12 @@ polizulia <- provea %>%
          grepl("Zulia", State) == TRUE) %>%
   mutate(polizulia = "Poli Zulia")
 
-police <- full_join(polisur, polizulia) %>%
-  mutate(group = ifelse(!is.na(polisur), "Poli Sur",
-                        ifelse(!is.na(polizulia), "Poli Zulia", NA))) %>%
-  replace(is.na(denuncias), 0)
+police_export <- full_join(polisur, polizulia) %>%
+  mutate(group = ifelse(!is.na(polisur), "Polisur",
+                        ifelse(!is.na(polizulia), "CPBEZ", NA)),
+         year = as.numeric(format(`End Date`, "%Y"))) 
+police_export$denuncias <- ifelse(is.na(police_export$denuncias), 0,
+                                  police_export$denuncias)
 
-bar_chart <- ggplot() +
-  geom_col(data = police,
-           mapping = aes(x = `Start Date`, y = denuncias,
-                         fill = group), position = "dodge") +
-  geom_text(data = police,
-            mapping = aes(x = as.POSIXct("31/03/2019", format = "%d/%m/%Y"),
-                          y = 10,
-                          label = "Prieto governor\nof Zulia"),
-            family = "Roboto",
-            hjust = 0) +
-  geom_text(data = police,
-            mapping = aes(x = as.POSIXct("31/03/2011", format = "%d/%m/%Y"),
-                          y = 10,
-                          label = "Prieto mayor\nof San Francisco"),
-            family = "Roboto",
-            hjust = 0) +
-  geom_text(data = police,
-            mapping = aes(x = as.POSIXct("01/10/2007", format = "%d/%m/%Y"),
-                          y = 35,
-                          label = "Abuses by Poli Sur and Poli Zulia"),
-            family = "Roboto Black", size = 6,
-            hjust = 0.09, color = "#3B3B3B") +
-  annotate("rect", xmin = as.POSIXct("10/12/2017", format = "%d/%m/%Y"),
-                          xmax = max(police$`End Date`),
-                          ymax = 20,
-                          ymin = 0,
-            fill = "#C53630", alpha = .2) +
-  annotate("rect", xmin = as.POSIXct("23/11/2008", format = "%d/%m/%Y"),
-           xmax = as.POSIXct("10/12/2017", format = "%d/%m/%Y"),
-           ymax = 20,
-           ymin = 0,
-           fill = "#374F98", alpha = .2) +
-  scale_y_continuous(breaks = c(5, 10, 15, 20, 150)) +
-  scale_fill_manual(values = c("#374F98", "#C53630")) +
-  xlab("") +
-  ylab("Cases of Abuse") +
-  theme(legend.position = c(.05, .2),
-        legend.direction = "horizontal",
-        axis.title.y = element_text(vjust = 0, hjust = 0, angle = 90),
-        plot.title = element_markdown()) +
-  hline
-bar_chart 
+write_excel_csv(police_export, "data/cleaned_data.csv")
+# additional cases added manually
